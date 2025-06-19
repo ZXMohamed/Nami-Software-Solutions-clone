f362fb24062c2a994d4068a84419b4d742d5df82
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-06-19 19:07:12 +0200

    contact

--- src/componants/contact.jsx
+++ src/componants/contact.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Box, Container, Grid, Stack, TextField, Typography } from '@mui/material'
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from '@mui/material'
 
 import gsap from 'gsap';
 import { SplitText } from 'gsap/SplitText';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form';
 
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -31,13 +35,35 @@ export default function Contact() {
         route:""
     }]);
     
-    const [inputhelptext, setinputhelptext] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        subject: "",
-        message:""
+    const zodmsgs = {
+        required: "This is a required field",
+        length: { less: (input, num)=>`${input} less than ${num} chars`, more: (input,num)=>`${input} more than ${num} chars` },
+        valid: (input) => `this ${input} is not valid`,
+    }
+    const regex = {
+        name:(name)=>name.match(/^[A-Za-z]+([ '-][A-Za-z]+)*$/),
+        phone:(phone)=>phone.match(/^\+?[0-9]{1,4}[-\s.]?(\(?\d{2,4}\)?[-\s.]?)?\d{3,4}[-\s.]?\d{4}$/) || phone.match(/^01[0125][0-9]{8}$/) || phone.match(/^\d{10,15}$/),
+    }
+    
+    const schema = zod.object({
+        name: zod.string().nonempty(zodmsgs.required).min(3, { message: zodmsgs.length.less("name",3) }).max(100, { message: zodmsgs.length.more("name",100) }).refine((name) => regex.name(name), { message: zodmsgs.valid("name") }),
+        subject: zod.string().nonempty(zodmsgs.required).min(3, { message: zodmsgs.length.less("subject",3) }).max(100, { message: zodmsgs.length.more("subject",100) }),
+        message: zod.string().nonempty(zodmsgs.required).max(500, { message: zodmsgs.length.more("message",500) }),
+        phone: zod.string().min(1, { message: zodmsgs.required }).refine((phone) => regex.phone(phone), { message: zodmsgs.valid("number") }),
+        email: zod.string().nonempty(zodmsgs.required).email(zodmsgs.valid)
     });
+
+    const { register, handleSubmit, formState:{errors, isSubmitting} } = useForm({resolver:zodResolver(schema),mode:"onChange"});
+    
+    const inputssettings = {
+        name: register("name", { required: true }),
+        subject: register("subject", { required: true }),
+        phone: register("phone", { required: true }),
+        email: register("email", { required: true }),
+        message: register("message", { required: true }),
+    }
+    
+
 
     const headertitle = useRef();
     const headersubtitle = useRef();
@@ -103,39 +129,36 @@ export default function Contact() {
         console.log("Captcha token:", token);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const onsubmit = (data) => {
         if (!captchaToken) {
-        alert("Please complete the CAPTCHA.");
-        return;
+        // alert("Please complete the CAPTCHA.");
+        
+        } else {console.log(data);
+            
+            // Proceed with form submission logic (e.g. send data to backend)
+            console.log("Form submitted with CAPTCHA:", captchaToken);
         }
 
-        // Proceed with form submission logic (e.g. send data to backend)
-        console.log("Form submitted with CAPTCHA:", captchaToken);
     };
 
-
-    function helptextshow(e,input) { 
-        setinputhelptext({...inputhelptext,[input]:(e.target.value=="" ?"This is a required field":"")})
-    }
 
   return (
     <Box className="contactsec">
         <Container maxWidth="lg">
             <Grid container rowSpacing={{xs:5,md:0}}>
                 <Grid size={{xs:12,md:6}} order={{xs:1,md:0}}>
-                    <Stack component={"form"} direction={"column"} spacing={6} className='contactformsec' onSubmit={handleSubmit} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150">
+                    <Stack component={"form"} direction={"column"} spacing={6} className='contactformsec' onSubmit={handleSubmit(onsubmit)} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150">
                         <Grid container rowSpacing={8} columnSpacing={4}>
-                            <Grid size={{xs:12,sm:6}}><TextField type={"text"}  color='secondary' label="Name" variant="standard" helperText={inputhelptext.name} onBlur={(e)=>helptextshow(e,"name")} /></Grid>
-                            <Grid size={{xs:12,sm:6}}><TextField type="email"  color='secondary'  label="Email" variant="standard" helperText={inputhelptext.email} onBlur={(e)=>helptextshow(e,"email")} /></Grid>
-                            <Grid size={{xs:12,sm:6}}><TextField type={"number"} color='secondary'   label="Phone" variant="standard" helperText={inputhelptext.phone} onBlur={(e)=>{helptextshow(e,"phone")}} /></Grid>
-                            <Grid size={ { xs: 12, sm: 6 } }><TextField type={ "Text" } color='secondary' label="Subject" variant="standard" helperText={ inputhelptext.subject } onBlur={(e)=>{helptextshow(e,"subject")}} /></Grid>
-                            <Grid size={ 12 }><TextField color='secondary'  label="Message" multiline rows={6} variant="standard" helperText={inputhelptext.message} onBlur={(e)=>{setinputhelptext({...inputhelptext,message:(e.target.value=="" ?"This is a required field":"")})}} /></Grid>
+                            <Grid size={{xs:12,sm:6}}><TextField type={"text"}  color='secondary' label="Name" variant="standard" helperText={errors?.name?.message} {...inputssettings.name} /></Grid>
+                            <Grid size={{xs:12,sm:6}}><TextField type="email"  color='secondary'  label="Email" variant="standard" helperText={errors?.email?.message} {...inputssettings.email} /></Grid>
+                            <Grid size={{xs:12,sm:6}}><TextField type={"number"} color='secondary'   label="Phone" variant="standard" helperText={errors?.phone?.message} {...inputssettings.phone} /></Grid>
+                            <Grid size={ { xs: 12, sm: 6 } }><TextField type={ "Text" } color='secondary' label="Subject" variant="standard" helperText={errors?.subject?.message} {...inputssettings.subject} /></Grid>
+                            <Grid size={ 12 }><TextField color='secondary'  label="Message" multiline rows={6} variant="standard" helperText={errors?.message?.message} {...inputssettings.message} /></Grid>
                         </Grid>
                         <ReCAPTCHA sitekey="6LdAk10rAAAAAKeGJg9mnA0wwBNtenRYAlp5da7e" onChange={handleCaptchaChange} />
                         <Stack direction={"row"} className='sendbuttoncon'>
                             <Box className="">
-                                <button type='submit'>Send</button>
+                                <Button variant='contained' disableRipple type='submit' disabled={isSubmitting}>Send</Button>
                             </Box>
                         </Stack>
                     </Stack>
