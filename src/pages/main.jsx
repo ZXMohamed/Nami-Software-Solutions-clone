91b976920844cf825b433a173df4ec894d92840b
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-09 01:18:56 +0200

    our-section

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import PageWrapper from '../componants/pagewrapper'
-import Home from '../componants/home'
 
 import "../sass/pages/main.scss"
 import { Our } from '../componants/about'
@@ -9,7 +8,6 @@ import { Our } from '../componants/about'
 export default function Main() {
   return (
     <PageWrapper>
-        <Home/>
         <Our/>
     </PageWrapper>
   )
