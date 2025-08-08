1102e55004eb51d0e8742c1a7210f0b02ae69ae9
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-09 00:57:33 +0200

    navbar

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,14 +2,11 @@
 import React from 'react'
 //*component
 import PageWrapper from '../componants/pagewrapper'
-import Home from '../componants/Home/Home'
-//*styles
-import "../sass/pages/main.scss"
 
 export default function Main() {
   return (
     <PageWrapper>
-      <Home/>
+
     </PageWrapper>
   )
 }
