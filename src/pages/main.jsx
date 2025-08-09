cc149f61bfd836598f0c10378f44b935e04922f7
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-09 02:47:19 +0200

    footer

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import PageWrapper from '../componants/pagewrapper'
-import Home from '../componants/home'
 
 import "../sass/pages/main.scss"
 import Footer from '../componants/footer'
@@ -9,7 +8,6 @@ import Footer from '../componants/footer'
 export default function Main() {
   return (
     <PageWrapper>
-      <Home />
       <Footer/>
     </PageWrapper>
   )
