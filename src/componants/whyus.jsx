97134c3153be9128b3e076edf049d089f1dc5e49
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-12 11:26:02 +0200

    whyus + style

--- src/componants/whyus.jsx
+++ src/componants/whyus.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Box, Container, Grid } from '@mui/material'
+import { Box, Container, Grid, Stack, Typography } from '@mui/material'
 import MiniHeader from './miniheader'
 import "../sass/shared/whyus.scss"
+import x from "../assets/photo/global/instagram.svg"
 
 export default function WhyUs() {
 
@@ -9,11 +10,11 @@ export default function WhyUs() {
         <>
             <Box dir={"ltr"} className="whyUsSection">
                 <MiniHeader dir={"ltr"} title={"Why us"} subtitle={"Why to choose work with us"}/>
-                <br/>
-                <br/>
                 <Container maxWidth="lg">
-                    <Grid container>
-                        <AnswerBox dir={"ltr"} data={{}} />
+                    <Grid container spacing={3}>
+                        <Grid size={ { sm: 4, xs: 12 } }>
+                            <AnswerBox dir={"ltr"} data={{icon:x, title:"Strong frame", description:"At Nami, we excel at using the latest technologies and advanced frameworks to develop high-performance and flexible websites."}} />
+                        </Grid>
                     </Grid>
                 </Container>
             </Box>
@@ -24,8 +25,10 @@ export default function WhyUs() {
 function AnswerBox({dir, data}) {
     
     return (
-        <Box>
-
-        </Box>
+        <Stack dir={dir} direction={"column"} className='answerBox'>
+            { data.icon && <img src={ data.icon } width={ 60 } height={ 60 } alt={ data.title + " service form nami" } className='answerBoxIcon' /> }
+            <Typography component={ "h5" } variant='h6' className='answerBoxTitle'>{ data.title }</Typography>
+            { data.description && <Typography className='answerBoxDescription'>{ data.description }</Typography> }
+        </Stack>
     )
 }
