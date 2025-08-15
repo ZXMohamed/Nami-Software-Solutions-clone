44e1cc89ade0636439cebc3bf3430f7f18395163
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-16 01:31:19 +0200

    AOS

--- src/componants/QA.jsx
+++ src/componants/QA.jsx
@@ -11,23 +11,32 @@ export default function QA() {
             <Box dir={"ltr"} className="QASection">
                 <MiniHeader dir={"ltr"} title={"FAQS"} subtitle={"The most common questions that clients ask about website development"}/>
                 <Container maxWidth="lg" className='QABoxCon'>
-                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} />
-                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} />
-                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} />
+                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} aosAnimation={boxAosAnimation}/>
+                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} aosAnimation={boxAosAnimation}/>
+                    <QABox dir={"ltr"} data={{Q:"What services does Nami provide?", A:"Nami offers a variety of services including website design and development, application development, cybersecurity services, and data analysis, in addition to digital marketing solutions and software support and maintenance."}} aosAnimation={boxAosAnimation}/>
                 </Container>
             </Box>
         </>
     )
 }
 
-function QABox({dir, data}) {
+function QABox({dir, data, aosAnimation}) {
     
     return (
-        <Accordion dir={dir} className='QABox'>
+        <Accordion dir={dir} className='QABox' {...boxAosAnimation}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />} className='QABox-Q'>
                 { data.Q }
             </AccordionSummary>
             <AccordionDetails className='QABox-A'>{data.A}</AccordionDetails>
         </Accordion>
     )
+}
+
+const aosAnimation = {
+  ["data-aos"]: "fade-up",
+  ["data-aos-duration"]: "1000",
+}
+const boxAosAnimation = {
+  ...aosAnimation,
+  ["data-aos-delay"]: "100"
 }
