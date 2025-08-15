52548f1ec6518cacfdd12d7e563b1fd01067ddb6
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-16 01:02:03 +0200

    cards AOS

--- src/componants/introcard.jsx
+++ src/componants/introcard.jsx
@@ -5,16 +5,33 @@ import { Stack, Typography } from '@mui/material'
 //*styles
 import "../sass/shared/introcard.scss"
 
-export default function IntroCard({ dir, icon, title, description, children = [], aosAnimation }) {
+export default function IntroCard({ dir, icon, title, description, children = [] }) {
   
     if (!title) return <></>;
 
     return (
-        <Stack dir={dir} direction={"column"} className='introCardCon' {...aosAnimation}>
+        <Stack dir={dir} direction={"column"} className='introCardCon' {...cardAosAnimation}>
             { icon && <img src={ icon } width={ 40 } height={ 40 } alt={ title + " service form nami" } className='introCardIcon' /> }
-            <Typography component={ "h1" } variant='h4' className='introCardTitle'>{ title }</Typography>
-            { description && <Typography className='introCardDescription'>{ description }</Typography> }
+            <Typography component={ "h1" } variant='h4' className='introCardTitle' {...titleAosAnimation} >{ title }</Typography>
+            { description && <Typography className='introCardDescription' {...descriptionAosAnimation} >{ description }</Typography> }
             {children}
         </Stack>
     )
+}
+
+const aosAnimation = {
+    ["data-aos"]: "fade-up",
+    ["data-aos-duration"]: "1000",
+}
+const cardAosAnimation = {
+    ...aosAnimation,
+    ["data-aos-delay"]: "50"
+}
+const titleAosAnimation = {
+    ...aosAnimation,
+    ["data-aos-delay"]: "100"
+}
+const descriptionAosAnimation = {
+    ...aosAnimation,
+    ["data-aos-delay"]: "150"
 }
--- src/componants/listcard.jsx
+++ src/componants/listcard.jsx
@@ -9,10 +9,19 @@ export default function ListCard({ dir, title, children = [] }) {
     if (!title) return <></>;
 
   return (
-    <Stack dir={dir} direction={"column"} className='listCardCon'>
+    <Stack dir={dir} direction={"column"} className='listCardCon' {...cardAosAnimation} >
         <Typography component={ "h2" } variant='h6' className='listCardTitle'>{ title }</Typography>
         <Divider />
         {children}
     </Stack>
   )
+}
+
+const aosAnimation = {
+    ["data-aos"]: "fade-up",
+    ["data-aos-duration"]: "1000",
+}
+const cardAosAnimation = {
+    ...aosAnimation,
+    ["data-aos-delay"]: "100"
 }
