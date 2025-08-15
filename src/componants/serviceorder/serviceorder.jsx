0241423542f4e2839430b73b204b8ad63ce820b9
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-15 04:12:37 +0200

    service-order no data no language no form handle

--- src/componants/serviceorder/serviceorder.jsx
+++ src/componants/serviceorder/serviceorder.jsx
@@ -5,20 +5,20 @@ import ListCard from '../listcard'
 import ObjectivesList from '../objectiveslist'
 import OrderService from './orderservice'
 
+import x from "../../assets/photo/global/facebook.svg"
+
 export default function ServiceOrder() {
   return (
     <Box>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
-          <Grid size={8}>
-            <IntroCard dir={"ltr"} icon={""} title={"Design services"} description={"We offer a comprehensive range of design services that include graphic design and brand identity design. We work to create innovative designs that reflect the essence of your brand and attract the attention of your audience."}/>
+          <Grid size={{md:8,xs:12}}>
+            <IntroCard dir={"ltr"} icon={x} title={"Design services"} description={"We offer a comprehensive range of design services that include graphic design and brand identity design. We work to create innovative designs that reflect the essence of your brand and attract the attention of your audience."}/>
           </Grid>
-          <Grid size={4}>
+          <Grid size={{md:4,xs:12}}>
             <ListCard dir={"ltr"} title={"Service objectives"}>
               <ObjectivesList dir={ "ltr" } data={ ["Graphic Design", "Brand identity design", "Logos design", "Marketing materials design", "User interface (UI) design"] } />
-              <br />
               <OrderService />
-              <br />
             </ListCard>
           </Grid>
         </Grid>
