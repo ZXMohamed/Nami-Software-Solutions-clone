76a4f3d57414610571a9bf8fa7e59c9fc7eddec6
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-16 05:03:57 +0200

    add wait skelton to elements

--- src/componants/introcard.jsx
+++ src/componants/introcard.jsx
@@ -5,37 +5,20 @@ import { Skeleton, Stack, Typography } from '@mui/material'
 //*styles
 import "../sass/shared/introcard.scss"
 
-export default function IntroCard({ dir, loading, icon, title, description, children = [] }) {
+export default function IntroCard({ dir, icon, title, description, children = [] }) {
   
     if (!title) return <></>;
 
     return (
         <>
-            { !loading &&
-                <Stack dir={ dir } direction={ "column" } className='introCardCon'>
-                    { icon && <img src={ icon } width={ 40 } height={ 40 } alt={ title + " service form nami" } className='introCardIcon' /> }
-                    <Typography component={ "h1" } variant='h4' className='introCardTitle' { ...titleAosAnimation } >{ title }</Typography>
-                    { description && <Typography className='introCardDescription' { ...descriptionAosAnimation } >{ description }</Typography> }
-                    { children }
-                </Stack>
-            }
-            { loading && <WaitItemsSkelton /> }
+            <Stack dir={ dir } direction={ "column" } className='introCardCon'>
+                { icon && <img src={ icon } width={ 40 } height={ 40 } alt={ title + " service form nami" } className='introCardIcon' /> }
+                <Typography component={ "h1" } variant='h4' className='introCardTitle' { ...titleAosAnimation } >{ title }</Typography>
+                { description && <Typography className='introCardDescription' { ...descriptionAosAnimation } >{ description }</Typography> }
+                { children }
+            </Stack>
         </>
     )
-}
-
-function WaitItemsSkelton() { 
-    return (
-        <>
-            <Stack direction={ "row" } justifyContent={ "space-between" } alignItems={ "center" }>
-                <Skeleton variant="rounded" width={ 60 } height={ 60 } />
-            </Stack>
-            <Skeleton variant="rounded" width={ 200 } height={ 20 } />
-            <Skeleton variant="rounded" width={ "100%" } height={ 10 } />
-            <Skeleton variant="rounded" width={ "100%" } height={ 10 } />
-            <Skeleton variant="rounded" width={ "80%" } height={ 10 } />
-        </>
-    );
 }
 
 const aosAnimation = {
--- src/componants/objectiveslist.jsx
+++ src/componants/objectiveslist.jsx
@@ -2,39 +2,17 @@ import { Box, Skeleton, Stack } from '@mui/material'
 import React from 'react'
 import "../sass/shared/objectiveslist.scss"
 
-export default function ObjectivesList({ dir, loading, data }) {
+export default function ObjectivesList({ dir, data }) {
 
     if (!data || !Array.isArray(data)) return <></>;
 
     return (
         <>
-            { !loading &&
-                <Stack dir={ dir } direction={ 'column' } className='objectivesList'>
-                    <ul className='serviceItemObjectives'>
-                        { data.map((objective, inx) => <li key={ inx }>{ objective }</li>) }
-                    </ul>
-                </Stack>
-            }
-            {loading && <WaitItemsSkelton/>}
+            <Stack dir={ dir } direction={ 'column' } className='objectivesList'>
+                <ul className='serviceItemObjectives'>
+                    { data.map((objective, inx) => <li key={ inx }>{ objective }</li>) }
+                </ul>
+            </Stack>
         </>
     )
-}
-
-
-
-function WaitItemsSkelton() { 
-    return (
-      <Box>
-        <Skeleton variant="rounded" width={ "100%" } height={ 15 } />
-        <br/>
-        <Skeleton variant="rounded" width={ "90%" } height={ 15 } />
-        <br/>
-        <Skeleton variant="rounded" width={ "100%" } height={ 15 } />
-        <br/>
-        <Skeleton variant="rounded" width={ "60%" } height={ 15 } />
-        <br/>
-        <Skeleton variant="rounded" width={ "80%" } height={ 15 } />
-        <br/>
-      </Box>
-    );
 }
