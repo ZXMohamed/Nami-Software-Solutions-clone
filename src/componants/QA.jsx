46f14fe12f87271024efdbd6c2e008cd060181a2
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-16 20:58:38 +0200

    service-order all done

--- src/componants/QA.jsx
+++ src/componants/QA.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import { Accordion, AccordionDetails, AccordionSummary, Box, Container, Skeleton, Typography } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MiniHeader from './miniheader'
 import "../sass/shared/QA.scss"
 import { useGetServicesQuestionQuery } from '../redux/server state/services';
+import { Language } from '../languages/languagesContext';
 
 export default function QA() {
 
+    const { isSuccess: language_isSuccess, data: language } = useContext(Language);
 
+    const defaultContent = useMemo(() => ({
+        direction: language_isSuccess ? language.page.direction : "ltr",
+        language: language_isSuccess ? language.page.language : "en",
+        title: language_isSuccess ? language.QA.title : "FAQS",
+        subtitle: language_isSuccess ? language.QA.subtitle : "The most common questions that clients ask about website development"
+    }), [language, language_isSuccess]);
 
     const { isError: servicesQuestion_isError, isSuccess: servicesQuestion_isSuccess, data: servicesQuestion } = useGetServicesQuestionQuery();
 console.log(servicesQuestion);
     return (
         <>
-            <Box dir={"ltr"} className="QASection">
-                <MiniHeader dir={"ltr"} title={"FAQS"} subtitle={"The most common questions that clients ask about website development"}/>
+            <Box dir={defaultContent.direction} className="QASection">
+                <MiniHeader dir={defaultContent.direction} title={defaultContent.title} subtitle={defaultContent.subtitle}/>
                 <Container maxWidth="lg">
                     { servicesQuestion_isSuccess && Object.values(servicesQuestion.QA).map((QA) => 
-                        <QABox key={QA.id} dir={ "ltr" } data={ QA } aosAnimation={ boxAosAnimation } />
+                        <QABox key={QA.id} dir={ defaultContent.direction } data={ QA } aosAnimation={ boxAosAnimation } />
                     )}
                     { !servicesQuestion_isSuccess && <QABoxWaitItemsSkelton/>}
                     { servicesQuestion_isError && <Typography variant='h5' color='error'>data not found !</Typography>}
