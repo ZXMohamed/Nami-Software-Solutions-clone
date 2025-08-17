a21cd8d6fe054207fc6028dde19f959583b6a0b6
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-17 05:37:20 +0200

    add why-us

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import PageWrapper from '../componants/pagewrapper'
 import ServiceOrder from '../componants/serviceorder/serviceorder'
 import "../sass/pages/service.scss"
+import WhyUs from '../componants/whyus'
 export default function Service() {
   return (
     <PageWrapper>
-      <ServiceOrder/>
+      <ServiceOrder />
+      <WhyUs />
+      
     </PageWrapper>
   )
 }
