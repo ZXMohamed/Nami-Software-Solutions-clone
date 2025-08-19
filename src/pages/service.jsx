82390e738addc14d88238737bf3baff6df564b07
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-19 22:07:31 +0200

    fix introcard naming

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PageWrapper from '../componants/pagewrapper'
-import ServiceOrder from '../componants/serviceorder/serviceorder'
+import ServiceOrderDetails from '../componants/serviceorder/serviceorderdetails'
 import "../sass/pages/service.scss"
 export default function Service() {
   return (
     <PageWrapper>
-      <ServiceOrder/>
+      <ServiceOrderDetails/>
     </PageWrapper>
   )
 }
