3be49ff792564a4910dddbaae8f78a9ca25747e2
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-25 05:25:29 +0200

    add blogs view

--- src/pages/blogspage.jsx
+++ src/pages/blogspage.jsx
@@ -2,11 +2,12 @@
 import React from 'react'
 //*component
 import PageWrapper from '../componants/pagewrapper'
+import BlogsView from '../componants/blogsview'
 
 export default function BlogsPages() {
   return (
     <PageWrapper>
-      
+      <BlogsView/>
     </PageWrapper>
   )
 }
