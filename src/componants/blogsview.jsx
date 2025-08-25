ffb9620b4af96d7cb94f21b1cad9b684a34fb327
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-25 05:23:09 +0200

    blogs view all done

--- src/componants/blogsview.jsx
+++ src/componants/blogsview.jsx
@@ -9,13 +9,13 @@ export default function BlogsView() {
     
     const defaultContent = useMemo(() => ({
         direction: language_isSuccess ? language.page.direction : "ltr",
-        subtitle: language_isSuccess ? language.subtitle : "Discover the world of articles together",
-        description: language_isSuccess ? language.description : "We offer unique and diverse content that meets readers’ interests and enhances their knowledge experiences. Our team of professional writers works passionately to craft engaging articles that inspire thought and enrich Arabic content. We always strive to develop innovative content that builds bridges of communication and knowledge. If you are a creative writer or passionate about the world of writing, join us and help make a difference in the world of Arabic content!",
+        subtitle: language_isSuccess ? language.header.subtitle : "Discover the world of articles together",
+        description: language_isSuccess ? language.header.description : "We offer unique and diverse content that meets readers’ interests and enhances their knowledge experiences. Our team of professional writers works passionately to craft engaging articles that inspire thought and enrich Arabic content. We always strive to develop innovative content that builds bridges of communication and knowledge. If you are a creative writer or passionate about the world of writing, join us and help make a difference in the world of Arabic content!",
     }), [language, language_isSuccess]);
 
     return (
         <Box>
-            <InfoCard dir={"ltr" } waveDir={"right" } subtitle={ defaultContent.subtitle } description={ defaultContent.description } animateDescription />
+            <InfoCard dir={"ltr" } waveDir={"right" } effects={ [infoCardEffects.sharpEffect] } typographyForm={ { subtitle: [typographyForm.subtitle.size.big] } }  subtitle={ defaultContent.subtitle } description={ defaultContent.description } animateDescription />
         </Box>
     )
 }
