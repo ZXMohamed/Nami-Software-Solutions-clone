0fa81a7afa90861e4ac35df28349666a1aa8fefe
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-08-25 10:57:32 +0200

    add routesbar

--- src/pages/projectdetails.jsx
+++ src/pages/projectdetails.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import PageWrapper from '../componants/pagewrapper'
+import RoutesBar from '../componants/routesbar';
+import ProjectShowDetails from '../componants/projectshowdetails/projectshowdetails';
 
 import "../sass/pages/projectdetails.scss"
 import 'swiper/css';
-import ProjectShowDetails from '../componants/projectshowdetails/projectshowdetails';
 
 export default function ProjectDetails() {
   return (
     <PageWrapper>
+      <RoutesBar/>
       <ProjectShowDetails />
       <br/>
       <br/>
