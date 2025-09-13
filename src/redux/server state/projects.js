4fdf2afb6404f8459d3f4ea7da803f8c0d686751
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-09-13 18:02:50 +0200

    fix portfolio data fetch + style

--- src/redux/server state/projects.js
+++ src/redux/server state/projects.js
@@ -9,12 +9,39 @@ const projectsSlice = createApi({
     tagTypes: ['ReQueryForMainPage'],
     endpoints: (builder) => ({
         getProjectsByCat: builder.query({
-            query: ({cat, count}) => "/query/projects.php?cat=" + cat + "&" + "count=" + count,
+            query: ({ cat, count, reset, search }) => "/query/projects.php?cat=" + cat + "&" + "count=" + count + "&" + (reset ? "reset=1" : "") + "&" + (search ? "search=" + search : ""),
             providesTags: ['ReQueryForMainPage']
         }),
+        getNextProjectsByCat: builder.query({
+            query: ({ cat, count, reset, search }) => "/query/projects.php?cat=" + cat + "&" + "count=" + count + "&" + (reset ? "reset=1" : "") + "&" + (search ? "search=" + search : ""),
+            serializeQueryArgs: ({ endpointName, queryArgs }) => {
+                // Cache key should include search + cat, but ignore count (page)
+                return `${endpointName}-${queryArgs.cat || ''}`//-${queryArgs.search || ''}-${queryArgs.reset || ''}-${queryArgs.cat || ''}`;
+            },
+            //Refetch when the page arg changes
+            // forceRefetch({ currentArg, previousArg }) {
+            //     return currentArg !== previousArg
+            // },
+            merge: (currentCache, newItems, { arg }) => {
+                console.log("ssss=>", currentCache, newItems);
+                
+                // if (arg.reset) {
+                //     // Start fresh if reset
+                //     return newItems;
+                // }
+
+                // Ensure currentCache is an array before spreading
+                return {...(currentCache || {}), ...newItems};
+            },
+            providesTags: ['ReQueryForMainPage']
+        }),
+        getCategories: builder.query({
+            query: ()=>"/query/portfoliocategory.php",
+            providesTags: ['ReQueryForMainPage']
+        })
     })
 })
 
 
 export default projectsSlice;
-export const { useGetProjectsByCatQuery } = projectsSlice;
+export const { useGetProjectsByCatQuery, useGetCategoriesQuery, useLazyGetNextProjectsByCatQuery } = projectsSlice;
