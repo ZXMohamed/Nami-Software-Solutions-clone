deee658831e438d7bd795e84797a63b70bd984d9
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-09-28 22:59:10 +0200

    folder organize + logic improve

---
+++ src/pages/service.jsx
@@ -0,0 +1,15 @@
+//*react
+import React from 'react'
+//*component
+import PageWrapper from '../componants/layouts/pagewrapper'
+import WhyUs from '../componants/sections/whyus'
+//*styles
+import "../sass/pages/service.scss"
+
+export default function Service() {
+  return (
+    <PageWrapper>
+      <WhyUs/>
+    </PageWrapper>
+  )
+}
