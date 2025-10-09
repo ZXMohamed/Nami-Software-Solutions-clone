f9f2c7fbaf6977ba60bf8fef3bd46cb88fd16415
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-10-10 01:59:36 +0200

    improve : page wrapper - language context - content delivary - alert + form organize - animation organize - db language toggle fix - folder orgainze

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+//*react
 import { Fragment } from "react"
+//*router
+import { BrowserRouter, Route, Routes } from "react-router"
+//*mui
 import { CssBaseline, ThemeProvider } from "@mui/material"
 //*init
 import "./aos/aosinit"
@@ -22,7 +26,12 @@ function App() {
     <Fragment>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Main/>
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/:language?/" element={ <Main/> } />
+            <Route path="*" element={ <h1>NOT FOUND !</h1> } /> {/*//!not found*/}
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </Fragment>
   )
---
+++ src/form/util/initZodMsgs.js
@@ -0,0 +1,27 @@
+export function initZodMsgs(language) {
+    if (language) {
+        const required = language.validationMsgs.required;
+        const length = language.validationMsgs.length;
+        const valid = language.validationMsgs.valid;
+        const unknown = language.validationMsgs.unknown;
+        const fileSize = language.validationMsgs.fileSize;
+        return {
+            required: required,
+            length: {
+                less: (input, num) => input + " " + length.less.middle + " " + num + " " + length.less.last,
+                more: (input, num) => input + " " + length.more.middle + " " + num + " " + length.more.last,
+            },
+            valid: (input) => valid.first + " " + input + " " + valid.last,
+            unknown: (input) => unknown.first + " " + input,
+            fileSize: (size) => fileSize.first + " " + size + fileSize.last
+        }
+    } else {
+        return {
+            required: "This is a required field",
+            length: { less: (input, num) => `${input} less than ${num} chars`, more: (input, num) => `${input} more than ${num} chars` },
+            valid: (input) => `this ${input} is not valid`,
+            unknown: (input) => `unknown ${input}`,
+            fileSize: (size) => `file is bigger than ${size}MB`
+        }
+    }
+}
