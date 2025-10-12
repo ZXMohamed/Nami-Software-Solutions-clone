2f23cdbd29da8ac9bc51e1169fa0841e5fb4141a
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-10-12 23:40:02 +0200

    improve : page wrapper - language context - content delivary - alert + form organize - animation organize - db language toggle fix - folder orgainze

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 //*component
 import PageWrapper from '../componants/layouts/pagewrapper'
-import Home from '../componants/sections/Home/Home'
 //*styles
 import "../sass/pages/main.scss"
 
 export default function Main() {
   return (
     <PageWrapper>
-      <Home/>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
     </PageWrapper>
   )
 }
