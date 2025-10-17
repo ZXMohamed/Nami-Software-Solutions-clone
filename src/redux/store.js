55a474e1cd14c7eb43a128262c3f7b727b796ba1
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-10-17 22:15:09 +0200

    image cash

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import languageSlice from "./server state/language";
 import projectsSlice from "./server state/projects";
+import imageCashSlice from "./clint state/imagecash";
 
 const store = configureStore({
     reducer: {
+        [imageCashSlice.name]: imageCashSlice.reducer,
         [languageSlice.reducerPath]: languageSlice.reducer,
         [projectsSlice.reducerPath]: projectsSlice.reducer
     },
