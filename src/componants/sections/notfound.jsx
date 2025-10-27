67f103c66d7783fbaac984258427cffc91860cb4
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-10-27 05:15:07 +0200

    not found page animation responsive

--- src/componants/sections/notfound.jsx
+++ src/componants/sections/notfound.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import { Link } from 'react-router'
+import { mainPageLinkAosAnimation, notFoundAosAnimation } from '../../animation/notfound'
 
 function NotFound() {
 
     return (
-        <Box className="notFoundCon">
-            <h1><span>404</span> <br />Page Not Found</h1>
-            <Link to={"/"}>Back to main page</Link>
+        <Box className="notFoundCon" >
+            <h1 {...notFoundAosAnimation}><span>404</span> <br />Page Not Found</h1>
+            <Link to={"/"} {...mainPageLinkAosAnimation}>Back to main page</Link>
         </Box>
     )
 }
