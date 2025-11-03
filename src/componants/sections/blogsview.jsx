fa6582dabd126b2d8f2ec57e727f3fd348d06036
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-03 08:41:33 +0200

    seo meta date json - db errors - alt lang

--- src/componants/sections/blogsview.jsx
+++ src/componants/sections/blogsview.jsx
@@ -1,5 +1,7 @@
 //*react
 import React from 'react'
+//*routes
+import { pages_routes } from '../../routes/routes';
 //*mui
 import { Box } from '@mui/material';
 //*components
@@ -7,6 +9,12 @@ import PageHead from '../shared/pagehead';
 import InfoCard, { infoCardEffects, typographyForm } from '../shared/infocard'
 //*hooks
 import { useContent } from '../../languages/hooks/usecontent';
+//*scripts
+import { defaultLanguage } from '../../languages/languagesContext';
+//*assets
+import logo from "../../assets/photo/global/namilogo.svg";
+
+
 
 export default function BlogsView() {
     
@@ -17,7 +25,12 @@ export default function BlogsView() {
             return {
                 direction: content.page.direction,
                 language: content.page.language,
-                logo: content.navBar.navLogo,
+                pageTitle:content.page.title,
+                meta: {
+                    title:content.page.meta.title,
+                    description:content.page.meta.description,
+                    image:content.page.meta.image,
+                },
                 subtitle: content.header.subtitle,
                 description: content.header.description,
             }
@@ -29,7 +42,7 @@ export default function BlogsView() {
 
     return (
         <Box>
-            <PageHead description={defaultContent.description} image={defaultContent.logo} language={defaultContent.language} url='/'/>
+            <PageHead pageTitle={defaultContent.pageTitle} title={defaultContent.meta.title} description={ defaultContent.meta.description } image={ defaultContent.meta.image } language={ defaultContent.language } url={ pages_routes(defaultContent.language)["blogs"].links } />
             <InfoCard dir={ defaultContent.direction } wave_dir={ "right" } effects={ [infoCardEffects.sharpEffect] } typographyForm={ { subtitle: [typographyForm.subtitle.size.big] } } subtitle={ defaultContent.subtitle } description={ defaultContent.description } animateDescription />
         </Box>
     )
@@ -37,6 +50,13 @@ export default function BlogsView() {
 
 const firstContent = {
     direction: "ltr",
+    language: defaultLanguage,
+    pageTitle: "blogs",
+    meta: {
+        title: "blogs | Nami Software Solutions",
+        description: "We offer unique and diverse content that meets readers’ interests and enhances their knowledge experiences. Our team of professional writers works passionately to craft engaging articles that inspire thought and enrich Arabic content. We always strive to develop innovative content that builds bridges of communication and knowledge. If you are a creative writer or passionate about the world of writing, join us and help make a difference in the world of Arabic content!",
+        image: logo,
+    },
     subtitle: "Discover the world of articles together",
     description: "We offer unique and diverse content that meets readers’ interests and enhances their knowledge experiences. Our team of professional writers works passionately to craft engaging articles that inspire thought and enrich Arabic content. We always strive to develop innovative content that builds bridges of communication and knowledge. If you are a creative writer or passionate about the world of writing, join us and help make a difference in the world of Arabic content!",
 }
