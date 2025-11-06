08845726aa00dd5799103623bab1e586d24702f9
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-06 05:40:21 +0200

    set env file

--- src/redux/server state/social.js
+++ src/redux/server state/social.js
@@ -3,7 +3,7 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const socialSlice = createApi({
     reducerPath: "social",
     baseQuery: fetchBaseQuery({
-        baseUrl:"https://192.168.1.100:443/nami-clone-data-api/"
+        baseUrl: import.meta.env.VITE_API_URL,
     }),
     endpoints: (builder) => ({
         getSocial: builder.query({
