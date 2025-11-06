574eb135693e0eb58bf7619d399d2b994c8a807e
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-06 05:36:33 +0200

    set env file

--- src/redux/server state/projects.js
+++ src/redux/server state/projects.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const projectsSlice = createApi({
     reducerPath: "projects",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://192.168.1.100:443/nami-clone-data-api/",
+        baseUrl: import.meta.env.VITE_API_URL,
         credentials: 'include'
     }),
     tagTypes: ['ReQueryForMainPage'],
