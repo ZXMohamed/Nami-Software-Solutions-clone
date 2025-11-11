5317fa48bab52db26517e182a7dccc76fee66199
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-11 05:32:06 +0200

    improve + fix errors

--- src/componants/shared/waveanimation.jsx
+++ src/componants/shared/waveanimation.jsx
@@ -1,17 +1,18 @@
 //*react
-import React from 'react'
+import React, { memo } from 'react'
 //*mui
 import { Box } from '@mui/material'
-
 //*styles
-import "../../sass/shared/infocard.scss"
+import "../../sass/shared/waveanimation.scss"
 
-export const WaveAnimation = ({ wave_dir }) => {
+export const WaveAnimation = memo(({ dir, wave_dir }) => {
   return (
-    <Box wave_dir={wave_dir} className="infoCardWaveAnimation">
+    <Box dir={dir} className="waveAnimationCon">
+      <Box dir={dir} wave_dir={ wave_dir } className="waveAnimation">
         <div></div>
         <div></div>
         <div></div>
+      </Box>
     </Box>
   );
-}
+});
