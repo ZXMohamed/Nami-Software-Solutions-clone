ef4f8f810f735c4e4684c8aefe35aedce2cde066
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-11 06:27:12 +0200

    improve + fix errors

--- src/componants/layouts/pagewrapper.jsx
+++ src/componants/layouts/pagewrapper.jsx
@@ -1,12 +1,7 @@
 //*react
 import React from 'react';
 //*component
-import PageTitle from '../shared/pagetitle';
 import NavBar from './navbar/navbar';
-// import FloatSocialButtons from './social&contacts/floatsocialbuttons';
-// import ContactButtons from './social&contacts/contactbuttons';
-// import ServicesTicker from './servicesticker';
-// import Footer from './footer';
 //*context
 import LanguagesProvider from '../../languages/languagesContext';
 
@@ -17,15 +12,8 @@ export default function PageWrapper({ children }) {
     <>
       <LanguagesProvider>
         
-        { console.log("sssssssssaaaaaaaaaaa") }
-        
-        <PageTitle/>
         <NavBar />
         {children}
-        {/* <FloatSocialButtons /> */}
-        {/* <ContactButtons /> */}
-        {/* <ServicesTicker /> */}
-        {/* <Footer /> */}
 
       </LanguagesProvider>
     </>
